c64ebfec22a1ab66630882eabbbf54fad3a0685d
gao mengnan <94237544@qq.com>
2023-09-12 11:24:04 +0800

    fix

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func main() {
 		log.Printf("Defaulting to port %s", port)
 	}
 
-	log.Printf("Listening on port %s", port)
+	log.Printf("Listening on port: %s", port)
 	log.Printf("Open http://localhost:%s in the browser", port)
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
 }
