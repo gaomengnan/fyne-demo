ad7dfec39af6e23583c2fe9df33cd712487d5300
gaomengnan <94237544@qq.com>
2023-09-13 14:47:03 +0800

    fixbug

--- main.go
+++ main.go
@@ -1,26 +1,39 @@
 package main
 
 import (
+	"fmt"
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/widget"
+	"os"
 )
 
 func main() {
-	a := app.New()
-	w := a.NewWindow("Hello")
-	w.Resize(fyne.Size{
-		Width:  500,
-		Height: 500,
-	})
-	hello := widget.NewLabel("Hello Fyne!")
-	w.SetContent(container.NewVBox(
-		hello,
-		widget.NewButton("Hi!", func() {
-			hello.SetText("Welcome :)")
-		}),
-	))
+	file, err := os.ReadFile("./Icon.png")
+	if err != nil {
+		return
+	}
+	myApp := app.New()
+	myWindow := myApp.NewWindow("Box Layout")
 
-	w.ShowAndRun()
+	myWindow.Resize(fyne.NewSize(500, 500))
+
+	// btn container
+	btn := widget.NewButtonWithIcon("点击我", fyne.NewStaticResource("logo", file), func() {
+		fmt.Println("press")
+	})
+	btnContainer := container.NewCenter(btn)
+	btnContainer.Resize(fyne.NewSize(50, 50))
+	btn.Resize(fyne.NewSize(50, 50))
+
+	myWindow.SetContent(btnContainer)
+
+	myWindow.ShowAndRun()
+
+	tidyUp()
+}
+
+func tidyUp() {
+	fmt.Println("Exited")
 }
